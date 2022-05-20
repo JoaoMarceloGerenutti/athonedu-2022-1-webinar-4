9c898c44cf78ed763549c6e9b14bbb88649b6753
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-05-20 15:24:23 -0300

    Creation of the Event Information screen, and its basic design.

---
+++ Xispirito/View/Registry/Registry.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Xispirito.View.Registry
+{
+    public partial class Registry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BackgroundEventImage.ImageUrl = "https://images.even3.com.br/eQN3chisIZIkC9DR62dAQCCkkCc=/1100x440/smart/even3.blob.core.windows.net/banner/lojjoooo.96d0c0dd63e34c139734.jpg";
+            EventImage.ImageUrl = "https://images.even3.com.br/eQN3chisIZIkC9DR62dAQCCkkCc=/1100x440/smart/even3.blob.core.windows.net/banner/lojjoooo.96d0c0dd63e34c139734.jpg";
+        }
+    }
+}
---
+++ Xispirito/View/Registry/Registry.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Xispirito.View.Registry
+{
+
+
+    public partial class Registry
+    {
+
+        /// <summary>
+        /// Controle BackgroundEventImage.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image BackgroundEventImage;
+
+        /// <summary>
+        /// Controle EventImage.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image EventImage;
+
+        /// <summary>
+        /// Controle EventTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EventTitle;
+
+        /// <summary>
+        /// Controle EventTime.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EventTime;
+
+        /// <summary>
+        /// Controle EventType.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EventType;
+
+        /// <summary>
+        /// Controle EventSubscribe.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button EventSubscribe;
+    }
+}
