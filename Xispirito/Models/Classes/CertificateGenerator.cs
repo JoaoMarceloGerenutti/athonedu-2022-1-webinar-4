620d410e23f4c42e67ca7b947d40398019f82790
João Marcelo Gerenutti <joaomarcelogerenuttii@hotmail.com>
2022-05-31 23:25:15 -0300

    Fixing pdf to png conversion method.

--- Xispirito/Models/Classes/CertificateGenerator.cs
+++ Xispirito/Models/Classes/CertificateGenerator.cs
@@ -11,18 +11,18 @@ namespace Xispirito.Models
     {
         public static void GenerateViewerCertificatePDF(Viewer viewer, Lecture lecture, Certificate certificate)
         {
-            //// Home.
-            //string outsideProjectPath = @"D:\Visual Studio\TCC Athon\2022-1-webinar-4\Xispirito\";
+            // Home.
+            string outsideProjectPath = @"D:\VisualStudio\TCCAthon\2022-1-webinar-4\Xispirito\";
 
-            // Work
-            string outsideProjectPath = @"C:\Users\MATHEUS\Desktop\TCC\2022-1-webinar-4\Xispirito\";
+            //// Work
+            //string outsideProjectPath = @"C:\Users\MATHEUS\Desktop\TCC\2022-1-webinar-4\Xispirito\";
 
             string insideProjectPath = @"View\Images\Certificates\";
             string path = @"Viewers\";
             string userEmail = viewer.GetEmail() + @"\";
-            string filename = Cryptography.GetMD5Hash(certificate.GetId().ToString());
+            string fileName = Cryptography.GetMD5Hash(certificate.GetId().ToString());
             string extension = ".pdf";
-            string fullPath = outsideProjectPath + insideProjectPath + path + userEmail + filename + extension;
+            string fullPath = outsideProjectPath + insideProjectPath + path + userEmail + fileName + extension;
 
             PdfDocument document = new PdfDocument();
             PdfPage page = document.AddPage();
@@ -78,7 +78,7 @@ namespace Xispirito.Models
             );
 
             // Certificate Key.
-            gfx.DrawString(Cryptography.GetMD5Hash(certificate.GetId().ToString()),
+            gfx.DrawString(fileName,
                 boldFont, XBrushes.Black,
                 new XRect(0, 0, page.Width, page.Height),
                 XStringFormats.BottomLeft
@@ -86,9 +86,12 @@ namespace Xispirito.Models
 
             document.Save(fullPath);
             SaveViewerCertificate(viewer.GetEmail(), certificate.GetId());
+            document.Close();
 
-            string outputPath = insideProjectPath + path + userEmail;
-            ConvertPdfToJpg(fullPath, outputPath, Cryptography.GetMD5Hash(certificate.GetId().ToString() + ".png"));
+            string inputPath = outsideProjectPath + insideProjectPath + path + userEmail + fileName;
+            string outputPath = inputPath;
+
+            ConvertPdfToPng(inputPath, outputPath);
         }
 
         private static void SaveViewerCertificate(string userEmail, int certificateId)
@@ -97,19 +100,17 @@ namespace Xispirito.Models
             viewerCertificateBAL.SaveViewerCertificate(userEmail, certificateId);
         }
 
-        private static void ConvertPdfToJpg(string inputFilePath, string outputFilePath, string outputFileName)
+        private static void ConvertPdfToPng(string inputFilePath, string outputFilePath)
         {
-            //ProcessStartInfo startInfo = new ProcessStartInfo("CMD.exe");
-            //startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            string arguments = string.Format(@"-density 300 {0}.pdf {1}.png", inputFilePath, outputFilePath);
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                Arguments = arguments,
+                FileName = @"D:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe"
+            };
+            startInfo.UseShellExecute = false;
 
-            //startInfo.WorkingDirectory = Path.GetFullPath(@"C:\Users\MATHEUS");
-
-            //startInfo.Arguments = "C/ dir";
-            ////startInfo.Arguments = "C/ magick convert -density 300 " + inputFilePath + " -resize 50% " + outputFilePath + outputFileName;
-
-            //Process.Start(startInfo);
-
-            Process.Start("cmd.exe", @"C/ dir");
+            Process.Start(startInfo).WaitForExit();
         }
     }
 }
